0c0fa963a7cde00ddab2cdd3bfab4e5c60ef9db2
ggdn <test@test.fr>
2018-10-29 11:57:17 +0100

    add config keycloak

--- src/main/java/lu/bcl/demo/TestController.java
+++ src/main/java/lu/bcl/demo/TestController.java
@@ -1,14 +1,19 @@
 package lu.bcl.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
@@ -30,8 +35,12 @@ public class TestController {
     }
 
     @GetMapping("/restricted")
-    public String restrictedArea() {
-        return "Access granted";
+    public Map<String, Object> restrictedArea(@AuthenticationPrincipal KeycloakAuthenticationToken authenticationToken) {
+        Map<String, Object> data = new LinkedHashMap<>();
+        data.put("username", authenticationToken.getName());
+        data.put("roles", AuthorityUtils.authorityListToSet(authenticationToken.getAuthorities()));
+        data.put("greeting", "Hello " + authenticationToken.getName() + " " + LocalDateTime.now());
+        return data;
     }
 
     @GetMapping(path = "/logout")
